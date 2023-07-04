e7a51a48196229b7adfc70bcd135566f070b69b0
Мария Рыкова <RykovaMarii@yandex.ru>
2023-07-04 09:40:30 +0300

    Создано консольное приложение для проверки работы Dll -библиотеки

---
+++ MainProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllLibrary;
+
+namespace MainProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team();
+        }
+    }
+}
