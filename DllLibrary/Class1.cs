e68fa6d5d031140d1e3178fcf47022552d8abe29
Мария Рыкова <RykovaMarii@yandex.ru>
2023-07-04 09:57:24 +0300

    Добавлены комментарии

--- DllLibrary/Class1.cs
+++ DllLibrary/Class1.cs
@@ -5,20 +5,25 @@ using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
+//Рыкова Мария 22ИС
 namespace DllLibrary
 {
+    //Реализован класс Команда
     public class Team
     {
+        //Свойства класса Команда
         public string Name { get; set; }
         public string Address { get; set; }
         public List<Player> Players = new List<Player>();
         
+        //Конструктор
         public Team(string name, string address)
         {
             Name = name;
             Address = address;
         }
 
+        //Метод, позволяющий вывести информацию о команде и игроках
         public void ShowAll()
         {
             Console.WriteLine($"Информация о команде:\n" + 
@@ -34,17 +39,21 @@ namespace DllLibrary
         }
     }
 
+    //Реализован класс Игрок
     public class Player : IComparable<Player>
     { 
+        //Свойства класса Игрок
         public string Name { get; set; }
         public string Description { get; set; }
 
+        //Конструктор
         public Player(string name, string description)
         {
             Name = name;
             Description = description;
         }
 
+        //Методы выода и сравнения класса Игрок
         public void Show()
         {
             Console.WriteLine($"- Имя игрока: {Name}\n"+ $"- Описание: {Description}\n");
--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -11,7 +11,17 @@ namespace MainProgram
     {
         static void Main(string[] args)
         {
-            Team team = new Team();
+            Team team = new Team("Крыски", "м. Бабушкинская");
+
+            team.Players.Add(new Player("Алина Пономарева", "Превосходный лидер команды"));
+            team.Players.Add(new Player("Светлана Стаханова", "Отменно играет"));
+            team.Players.Add(new Player("Захар Черниченко", "Ему нужен связующий"));
+            team.Players.Add(new Player("Андрей Сахаров", "Связующий"));
+            team.Players.Add(new Player("Мария Рыкова", "Боится мяча"));
+
+            team.ShowAll();
+
+            Console.ReadLine();
         }
     }
 }
