78fa61f15540ffdeb7978cb6cf5fca1cd5993fcf
Мария Рыкова <RykovaMarii@yandex.ru>
2023-07-04 09:24:22 +0300

    Cоздан первый класс "Team"

--- DllLibrary/Class1.cs
+++ DllLibrary/Class1.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DllLibrary
 {
-    public class Class1
+    public class Team
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Player> Players = new List<Player>();
+        
+        public Team(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine($"Информация о команде:\n" + 
+                $"Название: {Name}\n" +
+                $"Адрес: {Address}\n\n" +
+                "Состав команды:");
+
+            Players.Sort();
+            foreach (Player p in Players)
+            {
+                p.Show();
+            }
+        }
+    }
+
+    public class Player
+    { 
+
     }
 }
